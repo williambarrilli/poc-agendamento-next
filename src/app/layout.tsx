2bb4fc2acc0d86be0b45562f22ff04673178a4bc
William Barrilli <felip.ew@hotmail.com>
2023-07-29 22:33:53 -0300

    add initanalytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,12 +5,16 @@ import GlobalError from "./global-error";
 import { SessionProvider } from "next-auth/react";
 import Header from "@/share/components/header";
 import iconMR from "@/../public/icons/iconMR.svg";
+import { firebaseConfig } from "@/init-firebase";
+import { initializeApp } from "firebase/app";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  initializeApp(firebaseConfig);
+
   return (
     <html lang="pt-br">
       <head>
